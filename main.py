891d2070de3e3d02f8e3ba403510e447cc30810c
ytka <stasfyai73364@gmail.com>
2023-07-28 01:07:14 +0700

    'завершение'

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from funcs import pluc
+from funcs import minus
+
 
 number_one = input("Введите первое число")
 number_two = input("Введите второе число")
 
-summa = pluc(number_one, number_two)
+choice = int(input('''Введите номер операции
+Сложение - 1
+Вычитание - 2'''))
 
-print(summa)
+if choice == 1:
+    summa = pluc(number_one, number_two)
+    print(summa)
+elif choice == 2:
+    subtraction= minus(number_one, number_two)
+    print(subtraction)
+else:
+    print('неизвестная команда')
