8e8ae782b9e72288e03b5744bb46214e73f76edf
ytka <stasfyai73364@gmail.com>
2023-07-28 00:44:33 +0700

    'первая попытка'

---
+++ funcs.py
@@ -0,0 +1,8 @@
+def pluc(number_one, number_two):
+    summa = int(number_one) + int(number_two)
+    return summa
+
+
+number_one = input("Введите первое число")
+number_two = input("Введите второе число")
+print(pluc(number_one, number_two))
